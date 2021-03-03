586f86cd296ee5d4fe2f06ab320072cb544cbc0b
taiyaki <taichanf15eagle@gmail.com>
2021-03-03 23:39:53 +0900

    見た目の改善

--- scanner.py
+++ scanner.py
@@ -20,7 +20,6 @@ class Scanner:
             threads.append(thread)
         while True:
             if len(self.scan_result) == (self.max_port - self.min_port):
-                print('scan completed')
                 return self.scan_result
 
     def scan(self, port):
@@ -42,7 +41,10 @@ class PortInfo:
 if __name__ == '__main__':
     s = Scanner(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
     print(f'Port scan for {sys.argv[1]} had started.')
+    print(f'Scanning port {sys.argv[2]} to {sys.argv[3]}')
+    print('| PROTOCOL | PORT | STATUS |')
+    #print('----------------------------')
     res = s.run()
     res.sort(key=lambda x: x.port_no)
     for port_info in res:
-        print(f'{port_info.protocol}/{port_info.port_no} is {port_info.state}')
+        print('|{:10s}|{:6d}|{:>8s}|'.format(port_info.protocol, port_info.port_no, port_info.state))
